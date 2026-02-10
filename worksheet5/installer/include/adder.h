6912eebc2716bf2fa3c31fd309e0dc0f676eccc9
Your Name <you@example.com>
2026-02-10 14:48:20 +0000

    Worksheet 5: installer layout (bin lib include)

---
+++ worksheet5/installer/include/adder.h
@@ -0,0 +1,16 @@
+/**
+ * @file adder.h
+ * @brief Function declarations for simple maths.
+ */
+
+/**
+ * @brief Adds two integers.
+ * @param a First integer
+ * @param b Second integer
+ * @return The sum a+b
+ */
+
+
+#pragma once
+
+int add(int a, int b);
