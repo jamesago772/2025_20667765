43677849578283decf09b5930d4e6309478c3e59
Your Name <you@example.com>
2026-02-10 14:39:52 +0000

    Worksheet 5: simplify adder.h (no DLL macros)

--- worksheet5/adder.h
+++ worksheet5/adder.h
@@ -1,13 +1,16 @@
+/**
+ * @file adder.h
+ * @brief Function declarations for simple maths.
+ */
+
+/**
+ * @brief Adds two integers.
+ * @param a First integer
+ * @param b Second integer
+ * @return The sum a+b
+ */
+
+
 #pragma once
 
-#ifdef _WIN32
-  #ifdef maths_EXPORTS
-    #define MATHS_API __declspec(dllexport)
-  #else
-    #define MATHS_API __declspec(dllimport)
-  #endif
-#else
-  #define MATHS_API
-#endif
-
-MATHS_API int add(int a, int b);
+int add(int a, int b);
